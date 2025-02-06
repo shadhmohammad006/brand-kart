53732194bb42d2e867935a397bfc263260b02bb9
shadh <shadhmohammad006@gmail.com>
2025-02-06 15:09:09 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/brandee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.brandee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(child: Text('Hello, World! iam shaaadh',style: GoogleFonts.poppins(fontSize: 20,fontWeight: FontWeight.bold),)),
+    );
+  }
+}
